cf6229540a75190ce5f4c1e220657469389be296
Mahmood <mbmcroc@gmail.com>
2021-02-02 09:25:12 -0800

    Implementing chess engine

--- js/movegen.js
+++ js/movegen.js
@@ -105,26 +105,26 @@ function AddBlackPawnQuietMove(from, to) {
 	}
 }
 
-
+// Impelement second player here
 function GenerateMoves() {
 	brd_moveListStart[brd_ply + 1] = brd_moveListStart[brd_ply];
 	/*
 	var brd_ply; // alla halvdrag gjorda i sökträdet, om ply är 6 så har 46 drag gjorts
-	
+
 	say ply 1 loop all moves
 	for (index = brd_moveListStart[1]; index < brd_moveListStart[2]; ++index)
 		move = moveList[index];
-	
+
 	... use move
-	
+
 	brd_moveListStart[2] = brd_moveListStart[1];
 	AddMove(Move){
 	brd_moveList[brd_moveListStart[2]]= Move;
 	brd_moveListStart[2]++;
-	
+
 	}
 	*/
-	
+
 	var pceType;
 	var pceNum;
 	var pceIndex;
@@ -141,19 +141,19 @@ function GenerateMoves() {
 				if(RowBrd[sq] == ROWS.ROW_2 && brd_pieces[sq + 20] == PIECES.EMPTY) {
 					AddQuietMove(MOVE(sq,(sq+20),PIECES.EMPTY,PIECES.EMPTY,MFLAGPS));
 				}
-			} 
-			
+			}
+
 			if(SQOFFBOARD(sq + 9) == BOOL.FALSE && PieceCol[brd_pieces[sq + 9]] == COLOURS.BLACK) {
 				AddWhitePawnCaptureMove(sq, sq+9, brd_pieces[sq + 9]);
-			}  
+			}
 			if(SQOFFBOARD(sq + 11) == BOOL.FALSE && PieceCol[brd_pieces[sq + 11]] == COLOURS.BLACK) {
 				AddWhitePawnCaptureMove(sq, sq+11, brd_pieces[sq + 11]);
-			} 
-			
+			}
+
 			if(brd_enPas != SQUARES.NO_SQ) {
 				if(sq + 9 == brd_enPas) {
 					AddEnPassantMove(MOVE(sq,sq + 9,PIECES.EMPTY,PIECES.EMPTY,MFLAGEP));
-				} 
+				}
 				if(sq + 11 == brd_enPas) {
 					AddEnPassantMove(MOVE(sq,sq + 11,PIECES.EMPTY,PIECES.EMPTY,MFLAGEP));
 				}
@@ -166,7 +166,7 @@ function GenerateMoves() {
 				}
 			}
 		}
-		
+
 		if(brd_castlePerm & CASTLEBIT.WQCA) {
 			if(brd_pieces[SQUARES.D1] == PIECES.EMPTY && brd_pieces[SQUARES.C1] == PIECES.EMPTY && brd_pieces[SQUARES.B1] == PIECES.EMPTY) {
 				if(SqAttacked(SQUARES.E1,COLOURS.BLACK) == BOOL.FALSE && SqAttacked(SQUARES.D1,COLOURS.BLACK) == BOOL.FALSE ) {
@@ -174,32 +174,32 @@ function GenerateMoves() {
 				}
 			}
 		}
-		
+
 		pceType = PIECES.wN; // HACK to set for loop other pieces
-		
+
 	} else {
 		pceType = PIECES.bP;
 		for(pceNum = 0; pceNum < brd_pceNum[pceType]; ++pceNum) {
-			sq = brd_pList[PCEINDEX(pceType,pceNum)];			
-			
+			sq = brd_pList[PCEINDEX(pceType,pceNum)];
+
 			if(brd_pieces[sq - 10] == PIECES.EMPTY) {
 				AddBlackPawnQuietMove(sq, sq-10);
 				if(RowBrd[sq] == ROWS.ROW_7 && brd_pieces[sq - 20] == PIECES.EMPTY) {
 					AddQuietMove(MOVE(sq,(sq-20),PIECES.EMPTY,PIECES.EMPTY,MFLAGPS));
 				}
-			} 
-			
+			}
+
 			if(SQOFFBOARD(sq - 9) == BOOL.FALSE && PieceCol[brd_pieces[sq - 9]] == COLOURS.WHITE) {
 				AddBlackPawnCaptureMove(sq, sq-9, brd_pieces[sq - 9]);
-			} 
-			
+			}
+
 			if(SQOFFBOARD(sq - 11) == BOOL.FALSE && PieceCol[brd_pieces[sq - 11]] == COLOURS.WHITE) {
 				AddBlackPawnCaptureMove(sq, sq-11, brd_pieces[sq - 11]);
-			} 
+			}
 			if(brd_enPas != SQUARES.NO_SQ) {
 				if(sq - 9 == brd_enPas) {
 					AddEnPassantMove(MOVE(sq,sq - 9,PIECES.EMPTY,PIECES.EMPTY,MFLAGEP));
-				} 
+				}
 				if(sq - 11 == brd_enPas) {
 					AddEnPassantMove(MOVE(sq,sq - 11,PIECES.EMPTY,PIECES.EMPTY,MFLAGEP));
 				}
@@ -212,71 +212,71 @@ function GenerateMoves() {
 				}
 			}
 		}
-		
+
 		if(brd_castlePerm & CASTLEBIT.BQCA) {
 			if(brd_pieces[SQUARES.D8] == PIECES.EMPTY && brd_pieces[SQUARES.C8] == PIECES.EMPTY && brd_pieces[SQUARES.B8] == PIECES.EMPTY) {
 				if(SqAttacked(SQUARES.E8,COLOURS.WHITE) == BOOL.FALSE && SqAttacked(SQUARES.D8,COLOURS.WHITE) == BOOL.FALSE ) {
 					AddQuietMove(MOVE(SQUARES.E8, SQUARES.C8, PIECES.EMPTY, PIECES.EMPTY, MFLAGCA));
 				}
 			}
-		}	
-		
+		}
+
 		pceType = PIECES.bN; // HACK to set for loop other pieces
-	} 
-	
-	
+	}
+
+
 	pceIndex = LoopSlideIndex[brd_side];
 	pce = LoopSlidePce[pceIndex++];
-	while( pce != 0) {			
-		
+	while( pce != 0) {
+
 		for(pceNum = 0; pceNum < brd_pceNum[pce]; ++pceNum) {
 			sq = brd_pList[PCEINDEX(pce,pceNum)];
-			
+
 			for(index = 0; index < DirNum[pce]; ++index) {
 				dir = PceDir[pce][index];
 				t_sq = sq + dir;
-				
-				while(SQOFFBOARD(t_sq)==BOOL.FALSE) {				
-					
+
+				while(SQOFFBOARD(t_sq)==BOOL.FALSE) {
+
 					if(brd_pieces[t_sq] != PIECES.EMPTY) {
 						if( PieceCol[brd_pieces[t_sq]] == brd_side ^ 1) {
 							AddCaptureMove(MOVE(sq, t_sq, brd_pieces[t_sq], PIECES.EMPTY, 0));
 						}
 						break;
-					}	
+					}
 					AddQuietMove(MOVE(sq, t_sq, PIECES.EMPTY, PIECES.EMPTY, 0));
 					t_sq += dir;
 				}
 			}
-		}		
+		}
 		pce = LoopSlidePce[pceIndex++];
 	}
-	
+
 	pceIndex = LoopNonSlideIndex[brd_side];
 	pce = LoopNonSlidePce[pceIndex++];
-	
-	while( pce != 0) {	
-		
+
+	while( pce != 0) {
+
 		for(pceNum = 0; pceNum < brd_pceNum[pce]; ++pceNum) {
-			sq = brd_pList[PCEINDEX(pce,pceNum)];			
-			
+			sq = brd_pList[PCEINDEX(pce,pceNum)];
+
 			for(index = 0; index < DirNum[pce]; ++index) {
 				dir = PceDir[pce][index];
 				t_sq = sq + dir;
-				
-				if(SQOFFBOARD(t_sq) == BOOL.TRUE) {				    
+
+				if(SQOFFBOARD(t_sq) == BOOL.TRUE) {
 					continue;
-				}				
-			
+				}
+
 				if(brd_pieces[t_sq] != PIECES.EMPTY) {
 					if( PieceCol[brd_pieces[t_sq]] == brd_side ^ 1) {
 						AddCaptureMove(MOVE(sq, t_sq, brd_pieces[t_sq], PIECES.EMPTY, 0));
 					}
 					continue;
-				}	
+				}
 				AddQuietMove(MOVE(sq, t_sq, PIECES.EMPTY, PIECES.EMPTY, 0));
 			}
-		}				
+		}
 		pce = LoopNonSlidePce[pceIndex++];
 	}
 
